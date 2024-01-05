126cc58e24c673cc55fd1edaa13d6d03e33af955
Gex Bark <23gex23.reg@gmail.com>
2024-01-05 14:33:47 +0100

    Initial Commit

---
+++ RepoHunter/RepoHunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoHunterApp.swift
+//  RepoHunter
+//
+//  Created by Gergely Barka on 05/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct RepoHunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
